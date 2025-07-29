01f770f1125731a6d443410e5ae94c280d018e45
pavan <pavan@inncircles.com>
2025-07-29 22:00:47 +0530

    added custom queue for executor

--- thread_pool_demo.py
+++ thread_pool_demo.py
@@ -1,61 +1,56 @@
+import threading
 import concurrent.futures
-from token_bucket import TokenBucket
 import time
+from queue import Queue
+from token_bucket import TokenBucket
 
-# Simulates a worker task that requests a token, and if successful, runs for `duration` seconds.
-def worker_task(task_id: int, duration: int, bucket: TokenBucket):
-    # Try to acquire a token to proceed with execution
-    if bucket.acquire_token(task_id):
-        print(f"[Task {task_id}] executing...")
-        time.sleep(duration)
-        print(f"[Task {task_id}] completed")
-        return f"[Task {task_id}] result"
-    else:
-        """
-        If token couldn't be acquired 
-        (this can be implemented as a custom timeout per task or a fixed timeout 
-        whose logic resides in the acquire_token function)
-        """
-        print(f"[Task {task_id}] Couldnot get Token")
-        return None
+def worker_task(task_id: int, duration: int):
+    print(f"[Task {task_id}] executing...")
+    time.sleep(duration)
+    print(f"[Task {task_id}] completed")
+    return f"[Task {task_id}] result"
 
-# Task list with arrival delays and durations ( arrival at 0 means immediate arrival )
+def dispatcher(bucket: TokenBucket, task_queue: Queue, executor: concurrent.futures.ThreadPoolExecutor):
+    while True:
+        task = task_queue.get()
+        if task is None:
+            break 
+        task_id, duration = task["id"], task["duration"]
+
+        # Wait until token is available
+        if bucket.acquire_token(task_id):
+            executor.submit(worker_task, task_id, duration)
+
+# Input tasks with arrival times
 tasks = [
     {"id": 1, "arrival": 0, "duration": 3},
     {"id": 2, "arrival": 1, "duration": 2},
     {"id": 3, "arrival": 1, "duration": 1},
-    {"id": 4, "arrival": 2, "duration": 1},
-    {"id": 5, "arrival": 3, "duration": 1},
+    {"id": 4, "arrival": 3, "duration": 1},
+    {"id": 5, "arrival": 4, "duration": 1},
 ]
 
 # Sorting by arrival time so that the sleep logic works
 tasks.sort(key=lambda x: x["arrival"])
-
 start_time = time.time()
 
-with TokenBucket(capacity=5, refill_count=1, refill_interval=1, initial_token_count=0) as bucket:
+task_queue = Queue()
+with TokenBucket(capacity=5, refill_count=2, refill_interval=3, initial_token_count=0) as bucket:
     """
     Use a ThreadPoolExecutor to simulate concurrent task execution
     Max workers = 5 to allow up to 5 concurrent threads
     """
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        futures = []
+        # Start dispatcher in background
+        dispatch_thread = threading.Thread(target=dispatcher, args=(bucket, task_queue, executor), daemon=True)
+        dispatch_thread.start()
 
         for task in tasks:
             # calculate how long to sleep till the task arrives
             arrival = task["arrival"] - (time.time() - start_time)
             if arrival > 0:
                 time.sleep(arrival)
-            """
-            Submit the task to the executor
-            Tasks will start executing immediately if threads are available
-            """
-            futures.append(
-                executor.submit(worker_task, task["id"], task["duration"], bucket)
-            )
+            task_queue.put(task)
 
-        # Wait for all submitted tasks to complete and print results
-        for future in concurrent.futures.as_completed(futures):
-            result = future.result()
-            if result:
-                print(f"Completed: {result}")
+        task_queue.put(None) # to stop the dispatcher function
+        dispatch_thread.join()
